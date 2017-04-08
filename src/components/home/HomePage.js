6ee041faf7e78b7cf2bd902606511fe8024b08f5
R. Anthony Porter <rah@sheenonline.biz>
2017-04-08 17:59:25 -0400

    Added TODO list in README

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -7,7 +7,7 @@ class HomePage extends Component {
       <div className="jumbotron">
         <h1>Pluralsight Admin</h1>
         <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
-        <Link to="about" className="btn btn-primary btn-lg">Lean more</Link>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
       </div>
     );
   }
